a1226a32a1b2e4e9ce2f75775ed0c68fac4817ba
sanskar chouksey <sanskarchoukse468@gmail.com>
2026-01-28 15:09:59 +0530

    deployweb app

--- app.py
+++ app.py
@@ -110,4 +110,4 @@ def inject_datetime():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()    # ✅ THIS CREATES DB
-    app.run()    
+    app.run(debug=True, port=8000)    
